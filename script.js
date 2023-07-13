9dfcae36fa61d935187a3aa0a3c00723ac658072
carlos505 <aragonaugusto24@gmail.com>
2023-07-12 21:28:37 -0600

    Add new-book form and make delete btn work

--- script.js
+++ script.js
@@ -1,38 +1,94 @@
-let myLibrary = [
-    'The Great Gatsby',
-    'Harry Potter and the Philosopher\'s Stone',
-    'The Hunger Games',
-    'Game Of Thrones',
-    'The Power Of Now',
-    'JavaScript',
-    'HTML and CSS Guide',
-    'The Witcher',
-    'The Theory Of Everything',
-    'Alice In Wonderland',
-    'The Little Prince'
-]
-
+const bookFormEl = document.querySelector('#book-form')
 const bookShelf = document.querySelector('#book-shelf')
 
+const titleInput = document.querySelector('.title-input')
+const authorInput = document.querySelector('.author-input')
+const pagesInput = document.querySelector('.pages-input')
+const editionInput = document.querySelector('.edition-input')
+const readStatus = document.querySelector('input[type="checkbox"]')
+
+let myLibrary = []
+
+class BookInfo {
+    constructor(title, author, pages, edition, read) {
+        this.title = title
+        this.author = author
+        this.pages = pages
+        this.edition = edition
+        this.read = read
+    }
+}
+
+function addBookToLibrary(event) {
+    event.preventDefault()
+
+    const title = titleInput.value
+    const author = authorInput.value
+    const pages = pagesInput.value
+    const edition = editionInput.value
+    const read = readStatus.checked
+
+    const book = new BookInfo(title, author, pages, edition, read)
+
+    myLibrary.push(book)
+    showAllBooks()
+
+    newBookForm.style.display = 'none'
+    document.body.classList.remove('show-before')
+    bookFormEl.reset()
+}
+bookFormEl.addEventListener('submit', addBookToLibrary)
+
 function showAllBooks() {
-    for (let i = 0; i < myLibrary.length; i++) {
-        const book = document.createElement('div')
-        book.classList.add('book')
-        book.innerHTML = `
+    bookShelf.innerHTML = ''
+    myLibrary.forEach((book) => {
+        const bookEl = document.createElement('div')
+        bookEl.classList.add('book')
+        bookEl.innerHTML = `
             <h2>
-                ${myLibrary[i]}
+                Title: ${book.title}<br>
+                Author: ${book.author}<br>
+                Pages: ${book.pages}<br>
+                Edition: ${book.edition}<br>
+                Status: ${book.read ? 'Read' : 'Not Read'}
                 <span>
-                    <i class="fi fi-br-bookmark"></i>
-                    <i class="fi fi-sr-circle-book-open"></i>
-                    <i class="fi fi-sr-trash"></i>
+                    <i class="fi fi-br-bookmark save"></i>
+                    <i class="fi fi-sr-trash remove" data-index="${myLibrary.indexOf(book)}"></i>         
                 </span>
             </h2>
         `
-        bookShelf.appendChild(book)
-    }
+        bookShelf.appendChild(bookEl)
+    })
 }
-showAllBooks()
 
-// function Book() {} // Function constructor
+const addNewBook = document.querySelector('#add-new-book')
+const cancelBtn = document.querySelector('.cancel-button')
+const newBookForm = document.querySelector('.new-book-form')
 
-// function addBookToLibrary() {} 
+function showForm() {
+    newBookForm.style.display = 'block'
+    document.body.classList.add('show-before')
+}
+
+function hideForm() {
+    newBookForm.style.display = 'none'
+    document.body.classList.remove('show-before')
+}
+
+addNewBook.addEventListener('click', showForm)
+cancelBtn.addEventListener('click', hideForm)
+
+function removeBookFromLibrary(event) {
+    const btn = event.target
+    const index = parseInt(btn.dataset.index)
+
+    myLibrary.splice(index, 1)
+    showAllBooks()
+}
+
+bookShelf.addEventListener('click', (event) => {
+    if (event.target.classList.contains('remove')) {
+        removeBookFromLibrary(event)
+    }
+})
+
