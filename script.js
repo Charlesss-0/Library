a6bf7d1e3f3cca2b131c47fde2b730de443dc000
carlos505 <aragonaugusto24@gmail.com>
2023-07-13 12:45:24 -0600

    Add bookmarks section

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
 const bookFormEl = document.querySelector('#book-form')
 const bookShelf = document.querySelector('#book-shelf')
 
-const titleInput = document.querySelector('.title-input')
-const authorInput = document.querySelector('.author-input')
-const pagesInput = document.querySelector('.pages-input')
-const editionInput = document.querySelector('.edition-input')
-const readStatus = document.querySelector('input[type="checkbox"]')
-
 let myLibrary = []
+let bookmarkedBooks = []
 
 class BookInfo {
     constructor(title, author, pages, edition, read) {
@@ -21,6 +16,12 @@ class BookInfo {
 
 function addBookToLibrary(event) {
     event.preventDefault()
+
+    const titleInput = document.querySelector('.title-input')
+    const authorInput = document.querySelector('.author-input')
+    const pagesInput = document.querySelector('.pages-input')
+    const editionInput = document.querySelector('.edition-input')
+    const readStatus = document.querySelector('input[type="checkbox"]')
 
     const title = titleInput.value
     const author = authorInput.value
@@ -37,33 +38,50 @@ function addBookToLibrary(event) {
     document.body.classList.remove('show-before')
     bookFormEl.reset()
 }
+
+function addBookToBookmarks(event) {
+    const btn = event.target
+    const index = parseInt(btn.dataset.index)
+    const book = myLibrary[index]
+
+    bookmarkedBooks.push(book)
+
+    showBookmarks()
+}
+
 bookFormEl.addEventListener('submit', addBookToLibrary)
 
 function showAllBooks() {
     bookShelf.innerHTML = ''
-    myLibrary.forEach((book) => {
+    myLibrary.forEach((book, index) => {
         const bookEl = document.createElement('div')
         bookEl.classList.add('book')
         bookEl.innerHTML = `
-            <h2>
-                Title: ${book.title}<br>
-                Author: ${book.author}<br>
-                Pages: ${book.pages}<br>
-                Edition: ${book.edition}<br>
-                Status: ${book.read ? 'Read' : 'Not Read'}
+            <div>
+                <div id="book-info-wrapper">
+                    <p>Title: <span>${book.title}</span</p>
+                    <p>Author: <span>${book.author}</span></p>
+                    <p>Pages: <span>${book.pages}</span></p>
+                    <p>Edition: <span>${book.edition}</span></p>
+                    <p>Status: <span>${book.read ? 'Read' : 'Not Read'}</span></p>
+                </div>
                 <span>
-                    <i class="fi fi-br-bookmark save"></i>
-                    <i class="fi fi-sr-trash remove" data-index="${myLibrary.indexOf(book)}"></i>         
+                    <i class="fi fi-br-bookmark save" data-index="${index}"></i>
+                    <i class="fi fi-sr-trash remove" data-index="${index}"></i>         
                 </span>
-            </h2>
+            </div>
         `
         bookShelf.appendChild(bookEl)
     })
 }
 
 const addNewBook = document.querySelector('#add-new-book')
-const cancelBtn = document.querySelector('.cancel-button')
+const cancelBtn = document.querySelectorAll('.cancel-button')
 const newBookForm = document.querySelector('.new-book-form')
+
+const bookmarkEl = document.querySelector('#bookmarks')
+const favBooksEl = document.querySelector('.fav-books')
+const savedBooksEl = document.querySelector('.saved-books')
 
 function showForm() {
     newBookForm.style.display = 'block'
@@ -73,15 +91,44 @@ function showForm() {
 function hideForm() {
     newBookForm.style.display = 'none'
     document.body.classList.remove('show-before')
+    favBooksEl.style.display = 'none'
 }
 
+function showBookmarks() {
+    savedBooksEl.innerHTML = ''
+    bookmarkedBooks.forEach((book) => {
+        const bookmarkedBookEl = document.createElement('div')
+        bookmarkedBookEl.classList.add('book', 'saved-books-info')
+        bookmarkedBookEl.innerHTML = `
+            <div>
+                <div id="book-info-wrapper">
+                    <p>Title: <span>${book.title}</span></p>
+                    <p>Author: <span>${book.author}</span></p>
+                    <p>Pages: <span>${book.pages}</span></p>
+                    <p>Edition: <span>${book.edition}</span></p>
+                    <p>Status: <span>${book.read ? 'Read' : 'Not Read'}</span></p>
+                </div>
+                <span>
+                    <i class="fi fi-sr-bookmark save"></i>
+                </span>
+            </div>
+        `
+        savedBooksEl.appendChild(bookmarkedBookEl)
+    })
+}
+
+bookmarkEl.addEventListener('click', () => {
+    favBooksEl.style.display = 'block'
+    document.body.classList.add('show-before')
+})
 addNewBook.addEventListener('click', showForm)
-cancelBtn.addEventListener('click', hideForm)
+cancelBtn.forEach(btn => {
+    btn.addEventListener('click', hideForm)
+})
 
 function removeBookFromLibrary(event) {
     const btn = event.target
     const index = parseInt(btn.dataset.index)
-
     myLibrary.splice(index, 1)
     showAllBooks()
 }
@@ -89,6 +136,7 @@ function removeBookFromLibrary(event) {
 bookShelf.addEventListener('click', (event) => {
     if (event.target.classList.contains('remove')) {
         removeBookFromLibrary(event)
+    } else if (event.target.classList.contains('save')) {
+        addBookToBookmarks(event)
     }
 })
-
